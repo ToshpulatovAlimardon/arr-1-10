ae3c39d2f5a796f28b0085d1cb07bef7e2898191
Alimardon Toshpo'latov <alimardoncoder001@gmail.com>
2023-03-17 22:23:57 +0500

    added array 1-2-3

---
+++ script.js
@@ -0,0 +1,26 @@
+// Arr1
+// let arr = [];
+// let n = 11;
+// for(let i = 1; i <= n; i += 2) {
+//     arr.push(i);
+// }
+// console.log(arr);
+
+// Arr2
+// let arr = [];
+// let n = 5;
+// for(let i = 0; i <= n; i++) {
+//     let k = 2 ** i;
+//     arr.push(k);
+// }
+// console.log(arr);
+
+// Arr3
+let arr = [];
+let n = 12,
+    A = 3,
+    D = 2;
+for (let i = A; i <= n; i += D) {
+    arr.push(i);
+}
+console.log(arr);
